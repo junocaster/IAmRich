3f6efef5b085bb3cb94fd9f3b9518d6814baa08e
junocaster <yunisemadrinan@gmail.com>
2024-01-16 22:56:55 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() { runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[900],
+            title: Text('I Am Rich'),
+          ),
+          body: Center(
+           child: Image(
+             image: 
+             AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
